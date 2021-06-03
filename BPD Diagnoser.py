b7be2195b9d14d1ff47a0b75d3015e87608fe966
Alexander Delpin <alexander.delpin@upr.edu>
2021-06-03 15:05:33 -0400

    Added startup text and disclaimer

--- BPD Diagnoser.py
+++ BPD Diagnoser.py
@@ -1,1 +1,32 @@
 #START
+
+
+
+
+def print_start():
+    print("""
+
+ __    __     _                            _          _   _              ___    ___  ___      ___ _                                       
+/ / /\ \ \___| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___    / __\  / _ \/   \    /   (_) __ _  __ _ _ __   ___  ___  ___ _ __ 
+\ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | __| '_ \ / _ \  /__\// / /_)/ /\ /   / /\ / |/ _` |/ _` | '_ \ / _ \/ __|/ _ \ '__|
+ \  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/ / \/  \/ ___/ /_//   / /_//| | (_| | (_| | | | | (_) \__ \  __/ |   
+  \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__|_| |_|\___| \_____/\/  /___,'   /___,' |_|\__,_|\__, |_| |_|\___/|___/\___|_|   
+                                                                                                          |___/                           
+
+""")
+    print("-----------------------------------------------------------------------------------------------------------------------------------------")
+    print("DISCLAIMER:\n")
+    print("This program is not meant to properly or medically diagnose any mental illness or personality disorder.")
+    print("It is only meant to raise awareness of a serious personality disorder that affects millions of people all over the world.\n")
+    print("Please, follow up with proper medical advice if you or a loved one think they might have any kind of medical health disorder!")
+    print("-----------------------------------------------------------------------------------------------------------------------------------------")
+
+
+
+
+
+    
+
+
+
+print_start()
